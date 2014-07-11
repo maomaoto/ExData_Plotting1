312bfb92e57dea2ec9946a805c2fe96685841e9a
maomaoto <maomaoto@gmail.com>
2014-07-11 23:29:01 +0800

    Update plot1.R and add plot1.png

--- plot1.R
+++ plot1.R
@@ -1,7 +1,18 @@
 ## Coursera Exploratory Data Analysis
 ## Course Project 1
+## plot1.R
 
-# assume the data set is in same directory
+# assume the data set is in the working directory
 data <- read.table("./household_power_consumption.txt", sep=";", stringsAsFactors=FALSE, header=TRUE, na.strings="?")
 
-data <- data[,grepl("{01|02}/02/2007", data$)]
+# Get the observations of 2007-02-01 and 2007-02-02
+data <- data[grepl("^[12]/2/2007", data$Date),]
+
+# Create png device
+png(filename="plot1.png", width=480, height=480)
+
+# Draw histogram
+hist(data$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
+
+# Close png device
+dev.off()
